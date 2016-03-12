a1a55310a74843b89564a02ecf71901ef012b0b9
OceanOSdev <ThomasTNF@live.com>
2016-03-12 17:11:27 -0600

    Things kinda work

---
+++ WebApplication4/Models/Pendant.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.UI.HtmlControls;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Pendant
+    {
+        [Key]
+        public int Id { get; set; }
+        public decimal Price { get; }
+        public string Description { get; set; }
+        public Size PendantSize { get; set; }
+        public string PendantImage { get; set; }
+        public HttpPostedFileBase UploadedFile { get; set; }
+    }
+
+    public enum Size
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
